6e2eb9907d7c460aff265f5e7497dfe568bbb010
React.js <51733504+manartanveer@users.noreply.github.com>
2025-12-07 18:05:46 +0100

    Add files via upload
    Derived states and Tracking entries

--- DerivedState.jsx
+++ DerivedState.jsx
@@ -1,32 +1,28 @@
+
 import React, { useState } from 'react'
 
 function DerivedState() {
-  const [group, setGroup] = useState([]);
-  const [user, setUser] = useState("");
-
-  const AddToGroup = () => {
-    setGroup([...group, user]);
-        
-  };
-
+    const [ group, setGroup] = useState([]);
+    const [user, setUser] = useState("");
+    const AddToGroup=()=>{
+        setGroup([...group, user]);
+    }
+    const total = group.length;
+    const Unique = [new Set(group)].length;
+    const last = group[group.length-1]
   return (
     <div>
-      <h1>DerivedState</h1>
-
-      <input 
-        value={user} onChange={(event) => setUser(event.target.value)} 
-        type="text" placeholder="Enter value" 
-      />
-
-      <button onClick={AddToGroup}>AddToGroup</button>
-
-      <h3>Group List:</h3>
-      <ul>
-        {group.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-
+       <h1>DerivedState</h1>
+       <h2 className='bg-yellow-300 color-red text-red-500'>Total Entries :{total}</h2>
+       <h2 className='bg-yellow-300 color-red text-red-500'>Unique Enteries:{Unique} </h2>
+       <h2 className='bg-yellow-300 color-red text-red-500'>Last Entries {last}</h2>
+       <input className='text-green-500' type="text" placeholder='Enter user' onChange={(event)=>setUser(event.target.value)} />
+       <button onClick={AddToGroup}>AddUser</button>
+       {
+        group.map((item,index)=>(
+            <h1 key={index}>{item}</h1>
+        ))
+       }
     </div>
   )
 }
