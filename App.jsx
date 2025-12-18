22e2fbf85115caf96eccd10ac73e77111505a500
React.js <51733504+manartanveer@users.noreply.github.com>
2025-12-18 21:25:35 +0100

    Add files via upload
    Component Unmount, Mount, MountEnd

--- App.jsx
+++ App.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  let [name , setName]= useState ("Tanveer");
-  function changName(){
-    setName(prev =>prev === "Tanveer" ? "Kashan" : "Tanveer");
+  const [counter , setCounter] = useState(2);
+  
+  function whenMount(){
+    console.log("component is mounted");
   }
-  return (
-    <div>
+    function whenunMount(){
+    console.log("component is unmount");
+  }
+    function whenEnd(){
+    console.log("component is unmount");
+  }
 
-      <h1>{name}</h1>
-      <button onClick={changName}>click me</button>
+
+useEffect (()=>{  whenMount()},[])
+useEffect (()=>{  whenunMount()},[counter])
+useEffect (()=>{  
+  return ()=> whenEnd();
+},[counter])
+  
+  return (
+
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={()=>setCounter(counter + 2)}>Count</button>
+
     </div>
   )
 }
